a18713c00660ad01a87602ad8a9e0eec57802fe3
jimba tshetrim <jtshetrim@gmail.com>
2023-03-15 14:52:15 +0600

    feat: guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Your secret number is : {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32= match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too Less".red()),
+            Ordering::Equal => {
+                println!("{}", "You win".green());
+                break;
+            },
+            Ordering::Greater => println!("{}", "Too Big".red()),
+        };
+    };
 }
